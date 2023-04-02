d97c3819fb7d1406ca834b29438df54043544e3e
Ilker Kesen <ilkerksn@gmail.com>
2023-04-02 18:00:22 +0300

    implement other metrics

--- bin/eval.py
+++ bin/eval.py
@@ -1,8 +1,11 @@
 import json
 import click
 import torch
-from torchmetrics.functional.classification import multiclass_accuracy
+from torchmetrics.functional.classification import multiclass_accuracy, confusion_matrix, auroc
 from vl_bench.utils import process_path
+
+def format_score(val):
+    return round(100*val.item(), 2)
 
 
 @click.command()
@@ -16,7 +19,7 @@ from vl_bench.utils import process_path
     required=True,
 )
 def main(input_file, similarity):
-    using_probabilities =  not similarity
+    using_probabilities = not similarity
     input_file = process_path(input_file)
     with open(input_file, 'r') as f:
         pred = json.load(f)
@@ -26,11 +29,34 @@ def main(input_file, similarity):
     for idx, item in enumerate(pred.values()):
         scores[idx, :len(item['scores'])] = torch.tensor(item['scores'])
     labels = torch.zeros(num_examples, dtype=torch.long)
-    acc = multiclass_accuracy(
+    acc_r = multiclass_accuracy(
         scores, labels, num_classes=num_texts, average='micro')
-    click.echo(f'accuracy={round(100*acc.item(), 2)}%')
-    if using_probabilities:
-        import ipdb; ipdb.set_trace()
+    click.echo(f'acc_r={format_score(acc_r)}%')
+    if not using_probabilities:
+        return
+
+    caption_probs = scores[:, 0]
+    foil_probs = scores[:, 1:].flatten()
+    probs = torch.cat([caption_probs, foil_probs])
+    labels = torch.zeros(scores.numel(), dtype=torch.long)
+    labels[:num_examples] = 1
+    mat = confusion_matrix(probs, labels, task='binary')
+    TP = mat[1, 1]
+    FP = mat[0, 1]
+    TN = mat[0, 0]
+    FN = mat[1, 0]
+
+    p_c = PPV = format_score(TP / (TP+FP))
+    p_f = NPV = format_score(TN / (TN+FN))
+    acc = format_score((TP + TN) / (TP + TN + FP + FN))
+    auroc_val = format_score(auroc(probs, labels, task='binary'))
+
+    print(f'p_c={p_c}%')
+    print(f'p_f={p_f}%')
+    print(f'min(p_c, p_f)={min(p_c, p_f)}%')
+    print(f'acc={acc}%')
+    print(f'auroc={auroc_val}')
+
 
 
 if __name__ == "__main__":
